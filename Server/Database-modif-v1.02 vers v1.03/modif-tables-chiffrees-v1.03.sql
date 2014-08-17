0588878afe1ffe7d18f8875159db2f4d80665377
Sylvain <sylvain.werdefroy@gmail.com>
2014-08-17 15:17:06 +0200

    v1.03 (suite)

---
+++ Server/Database-modif-v1.02 vers v1.03/modif-tables-chiffrees-v1.03.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `config` ADD `withIdPwd` tinyint (1) DEFAULT '0';
+ALTER TABLE `config` ADD `id1Value` varbinary(64) DEFAULT NULL;
+ALTER TABLE `config` ADD `id2Value` varbinary(64) DEFAULT NULL;
+ALTER TABLE `config` ADD `id3Value` varbinary(64) DEFAULT NULL;
+ALTER TABLE `config` ADD `id4Value` varbinary(64) DEFAULT NULL;
+ALTER TABLE `config` ADD `pwdValue` varbinary(64) DEFAULT NULL;
