7efc68e0b97cd8dcf5401b68ee957d8a2217795f
swSSO <sylvain.werdefroy@gmail.com>
2016-09-08 17:50:38 +0200

    1.12

--- swSSO/swSSOMain.cpp
+++ swSSO/swSSOMain.cpp
@@ -38,8 +38,8 @@
 #include "ISimpleDOMDocument_i.c"
 
 // Un peu de globales...
-const char gcszCurrentVersion[]="111";	// 101 = 1.01
-const char gcszCurrentBeta[]="1124";	// 1021 = 1.02 beta 1, 0000 pour indiquer qu'il n'y a pas de beta
+const char gcszCurrentVersion[]="112";	// 101 = 1.01
+const char gcszCurrentBeta[]="0000";	// 1021 = 1.02 beta 1, 0000 pour indiquer qu'il n'y a pas de beta
 
 HWND gwMain=NULL;
 
