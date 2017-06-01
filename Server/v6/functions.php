d35037672734bcecaf67a6337851ebbdf6cc988f
swSSO <sylvain.werdefroy@gmail.com>
2017-06-01 09:34:15 +0200

    1.15B3

--- Server/v6/functions.php
+++ Server/v6/functions.php
@@ -1,33 +1,21 @@
 <?php
 //-----------------------------------------------------------------------------
-//
 //                                  swSSO
-//
-//       SSO Windows et Web avec Internet Explorer, Firefox, Mozilla...
-//
 //                Copyright (C) 2004-2017 - Sylvain WERDEFROY
-//
-//							 http://www.swsso.fr
-//                   
+//                https://www.swsso.fr | https://www.swsso.com
 //                             sylvain@swsso.fr
-//
 //-----------------------------------------------------------------------------
-// 
 //  This file is part of swSSO.
-//  
 //  swSSO is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//
 //  swSSO is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//
 //  You should have received a copy of the GNU General Public License
 //  along with swSSO.  If not, see <http://www.gnu.org/licenses/>.
-// 
 //-----------------------------------------------------------------------------
 // VERSION INTERNE : 6.5.1
 // - ajout de la colonne autoPublish
--- Server/v6/webservice6.php
+++ Server/v6/webservice6.php
@@ -1,37 +1,21 @@
 <?php
-include('variables.php');
-include('util.php');
-include('functions.php');
-include('sessions.php');
 //-----------------------------------------------------------------------------
-//
 //                                  swSSO
-//
-//       SSO Windows et Web avec Internet Explorer, Firefox, Mozilla...
-//
 //                Copyright (C) 2004-2017 - Sylvain WERDEFROY
-//
-//							 http://www.swsso.fr
-//                   
+//                https://www.swsso.fr | https://www.swsso.com
 //                             sylvain@swsso.fr
-//
 //-----------------------------------------------------------------------------
-// 
 //  This file is part of swSSO.
-//  
 //  swSSO is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//
 //  swSSO is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//
 //  You should have received a copy of the GNU General Public License
 //  along with swSSO.  If not, see <http://www.gnu.org/licenses/>.
-// 
 //-----------------------------------------------------------------------------
 // webservice6.PHP : Utilisé à partir de la version swSSO 1.05 pour la gestion des domaines multiples.
 //                   Le client 1.05 et les suivants resteront compatibles avec webservice5.php tant
@@ -50,6 +34,10 @@ include('sessions.php');
 // - modification de la fonction getconfigautopublish
 // - ajout de la fonction getdomainconfigsautopublish
 //------------------------------------------------------------------------------
+include('variables.php');
+include('util.php');
+include('functions.php');
+include('sessions.php');
 
 $swssoVersion="000:0000"; // "000:0000" désactive le contrôle de version côté client
 
